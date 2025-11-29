f966759b6494429e7c851890f8d3f81fee812090
JorisSTG <joris.stg@gmail.com>
2025-11-29 01:40:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -237,7 +237,9 @@ if uploaded:
             "Précision (%)": pct_precision
         })
 
-    df_temp_precision = pd.DataFrame(results_temp)
+    df_temp_precision_styled = df_temp_precision.style \
+    .background_gradient(subset=["Précision (%)"], cmap="RdYlGn", axis=None)
+    st.dataframe(df_temp_precision_styled, hide_index=True)
     st.subheader(f"Précision du modèle sur la répartition des durées des plages de température (TRACC +{scenario_sel}/{ville_sel})")
     st.dataframe(df_temp_precision, hide_index=True)
 
