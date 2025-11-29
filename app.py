80891123abe671d0e6b30dd49a72d0a21d60027e
JorisSTG <joris.stg@gmail.com>
2025-11-29 13:16:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,10 +29,10 @@ percentiles_list = [5, 25, 50, 75, 95]
 
 # -------- Noms des mois --------
 mois_noms = {
-    1: "Janvier",   2: "Février",  3: "Mars",
-    4: "Avril",     5: "Mai",      6: "Juin",
-    7: "Juillet",   8: "Août",     9: "Septembre",
-    10: "Octobre", 11: "Novembre", 12: "Décembre"
+    1: "1 - Janvier",   2: "2 - Février",  3: "3 - Mars",
+    4: "4 - Avril",     5: "5 - Mai",      6: "6 - Juin",
+    7: "7 - Juillet",   8: "Août",     9: "Septembre",
+    10: "10 - Octobre", 11: "11 - Novembre", 12: "12 - Décembre"
 }
 
 # -------- Choix scénario et ville --------
