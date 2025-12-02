5892e0cc7ed17739e395f24839c8cd36a2b382a8
JorisSTG <joris.stg@gmail.com>
2025-12-02 23:06:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -547,10 +547,12 @@ if uploaded:
     
     results_djc = []
     results_djf = []
-    mois_noms_sans_num = [
-        "janvier", "février", "mars", "avril", "mai", "juin",
-        "juillet", "août", "septembre", "octobre", "novembre", "décembre"
-    ]
+    mois_noms_sans_num = {
+    1: "Janvier",   2: "Février",  3: "Mars",
+    4: "Avril",     5: "Mai",      6: "Juin",
+    7: "Juillet",   8: "Août",     9: "Septembre",
+    10: "Octobre", 11: "Novembre", 12: "Décembre"
+}
 
     for mois_num in range(1, 13):
         mois = mois_noms_sans_num[mois_num]
