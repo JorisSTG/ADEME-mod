5e0283dbf94e528f3443b8c148f2004d2cacdf4a
JorisSTG <joris.stg@gmail.com>
2025-12-04 12:06:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -372,10 +372,10 @@ if uploaded:
     heures_TRACC_froid = np.sum(obs_hourly_annual < tn_seuil_froid)
     heures_modele_froid = np.sum(mod_hourly_annual < tn_seuil_froid)
     
-    if heures_TRACC_froid < heures_modele_chaud:
-        phrase_tn_froid = f"Le modèle a plus d'heures avec une T>{tn_seuil_froid}°C ({heures_modele_froid}) que TRACC ({heures_TRACC_froid})."
+    if heures_TRACC_froid > heures_modele_chaud:
+        phrase_tn_froid = f"Le modèle a plus d'heures avec une T<{tn_seuil_froid}°C ({heures_modele_froid}) que TRACC ({heures_TRACC_froid})."
     else:
-        phrase_tn_froid = f"TRACC a plus d'heures avec une T>{tn_seuil_froid}°C ({heures_TRACC_froid}) que le modèle ({heures_modele_froid})."
+        phrase_tn_froid = f"TRACC a plus d'heures avec une T<{tn_seuil_froid}°C ({heures_TRACC_froid}) que le modèle ({heures_modele_froid})."
 
     # Stocker dans st.session_state pour la page Résumé
     st.session_state["resume_hist"] = [phrase_tx_chaud, phrase_tn_froid]
